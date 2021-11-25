a5c4e14cb6ee7a4fc6cc623d7b2b424b29d61c1c
bumyy32 <bumyy32@gmail.com>
2021-11-25 10:53:39 -0300

    Adicionado lista de estados usando API IBGE (WIP)
    Modificado no JS
    Código:
    (function(){
      var url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
      var select = document.getElementById('estado');
      var options = '<option>Selecione</option>';
    
      select.innerHTML = options;
    })();
    
    ///WIP///

--- js/code.js
+++ js/code.js
@@ -1,2 +1,19 @@
 // Inicializa a biblioteca de animação Aos
   AOS.init();
+
+  // teste
+  // function teste(){
+  //   alert('Olá Mundo!')
+  // }
+  
+  // document.getElementById('inicio').addEventListener('click', teste);
+
+// Função Encapsulada: Preenche o SELECT com os dados da APi do IBGE
+(function(){
+  var url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
+  var select = document.getElementById('estado');
+  var options = '<option>Selecione</option>';
+
+  select.innerHTML = options;
+})();
+
