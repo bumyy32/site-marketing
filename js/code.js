0b546567fdb576b540d2fca0114258b39d396014
bumyy32 <bumyy32@gmail.com>
2021-11-25 11:18:54 -0300

    Update no code.js usando fetch
    Começamos a usar o fetch
    estrutura:
    fetch().then().catch();
    sendo fetch = url
    then = ação caso der certo
    catch = ação caso de errado

--- js/code.js
+++ js/code.js
@@ -1,6 +1,6 @@
 // Inicializa a biblioteca de animação Aos
   AOS.init();
-
+  
   // teste
   // function teste(){
   //   alert('Olá Mundo!')
@@ -13,6 +13,13 @@
   var url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
   var select = document.getElementById('estado');
   var options = '<option>Selecione</option>';
+
+  // consulta a api com o método fetch e com um laço de repetição incrementa
+  // as tags option
+  // fetch().then().catch();
+  // O primeiro then captura os dados, e o segundo trata dos dados, e catch é o tratamento do erro, ações.
+  fetch(url).then(response => response.json()).then(json => console.log(json)).catch();
+
 
   select.innerHTML = options;
 })();
